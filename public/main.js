8f096aa77ef5b8b19800ca951befd4363f57a083
ArturoUrbina <61171840+ArturoUrbina@users.noreply.github.com>
2020-12-16 15:43:40 -0600

    Chat

---
+++ public/main.js
@@ -0,0 +1,26 @@
+const socket = io();
+
+socket.on('messages', function(data) {
+  render(data);
+})
+
+function render (data) {  
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {  
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
